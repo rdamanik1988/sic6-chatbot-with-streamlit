a68ee426a5ee040568d15ffc0020a6fcb2291c3a
Ronal Damanik <88766790+rdamanik1988@users.noreply.github.com>
2025-08-17 18:20:04 +0700

    Update indexing.py
    update indexing menyesuaikan dengan huggingface embedding agar sesuai

--- utilities/indexing.py
+++ utilities/indexing.py
@@ -4,16 +4,21 @@ from langchain.text_splitter import CharacterTextSplitter
 from langchain_community.docstore.in_memory import InMemoryDocstore
 from langchain_community.vectorstores import FAISS
 from langchain_core.documents import Document
-from langchain_google_genai import GoogleGenerativeAIEmbeddings
 from uuid import uuid4
 from PyPDF2 import PdfReader
+from langchain_community.embeddings import HuggingFaceBgeEmbeddings
 
 class Index:
     def __init__(self, pdf):
-        embeddings = GoogleGenerativeAIEmbeddings(model="models/text-embedding-004")
-        index = faiss.IndexFlatL2(len(embeddings.embed_query("hello world"))) #untuk mendapatkan length vectornya
+        # Mengganti GoogleGenerativeAIEmbeddings dengan HuggingFaceBgeEmbeddings
+        self.embeddings = HuggingFaceBgeEmbeddings(model_name="BAAI/bge-small-en-v1.5")
+        
+        # Mendapatkan panjang vektor secara dinamis dari model embedding baru
+        vector_length = len(self.embeddings.embed_query("hello world"))
+        
+        index = faiss.IndexFlatL2(vector_length)
         self.vector_store = FAISS(
-                embedding_function=embeddings,
+                embedding_function=self.embeddings,
                 index=index,
                 docstore=InMemoryDocstore(),
                 index_to_docstore_id={},
@@ -39,4 +44,3 @@ class Index:
         
     def save_index(self):
         self.vector_store.save_local("faiss_index")
-    
